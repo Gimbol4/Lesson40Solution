ea074374040ec236a52b506427f64e7dd5e10a65
Gimbol4 <Shishkanov.yd@gmail.com>
2026-02-11 19:56:17 +0300

    Update

--- Task01Project/main3.cpp
+++ Task01Project/main3.cpp
@@ -1,11 +1,29 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <string>
+
 
 using namespace std;
 
-char bar(int a) {
+//string bar(int a) {
+//	string s = "";
+//	while (a > 0) {
+//		s = s + (char)254;
+//		a--;
+//	}
+//	return s;
+//}
 
+string symbol(int a) {
+	string s = "";
+	int code = 0;
+	while (code < a) {
+		s += to_string(code) + " - " + (char)code + '\n';
+		code++;
+	}
+	return s;
 }
+
 
 int main() {
 	int a;
@@ -14,12 +32,8 @@ int main() {
 		cout << "Input a postive number: ";
 		cin >> a;
 	} while (a <= 0);
-
-	while (a >= 0) {
-		system("cls");
-		cout << a;
-		a--;
-		Sleep(500);
-	}
+	cout << symbol(a) << (char)176;
+	//while (3)
+	//}
 	return 0;
-}//□ ■
+}//□ ■░
