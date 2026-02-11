413cd7bb8469ba559e08ea10cc77f8dbef106ee7
Gimbol4 <Shishkanov.yd@gmail.com>
2026-02-11 19:29:32 +0300

    ru

--- Task01Project/main02.cpp
+++ Task01Project/main02.cpp
@@ -1,35 +1,26 @@
-#include <iostream>
-#include <Windows.h>
-
-using namespace std;
-
-int reverse(int a);
-
-int main() {
-	int a;
-	do {
-		system("cls");
-		cout << "Input a postive number: ";
-		cin >> a;
-	} while (a <= 0);
-
-	while (a >= 0) {
-		system("cls");
-		cout << a;
-		a--;
-		Sleep(500);
-	}
-	system("cls");
-	cout << "Complete.";
-	return 0;
-}
-
-int reverse(int a) {
-	int res = 0;
-	while (a > 0) {
-		res = res * 10;
-		res = res + (a % 10);
-		a = a / 10;
-	}
-	return res;
-}
+//#include <iostream>
+//#include <Windows.h>
+//
+// Это коментарий на русском
+//using namespace std;
+//
+//int reverse(int a);
+//
+//int main() {
+//	int a;
+//	do {
+//		system("cls");
+//		cout << "Input a postive number: ";
+//		cin >> a;
+//	} while (a <= 0);
+//
+//	while (a >= 0) {
+//		system("cls");
+//		cout << a;
+//		a--;
+//		Sleep(500);
+//	}
+//	system("cls");
+//	cout << "Complete.";
+//	return 0;
+//}
